9004dd7228f375392050f3d0b753563f11d6e074
monodrone <paul@lada.kefir>
2019-05-23 22:43:56 -0500

    Removed classy complications

--- Zarya.py
+++ Zarya.py
@@ -9,30 +9,6 @@ import tkinter as tk
 from tkinter import * 
 
 
-print("\033[1;34mSTACK\033[0m\n")
-
-class Stack:
-    def __init__(self):
-      self.stack = []
-      
-    # Use list append method to add element
-    def add(self, dataval):
-      if dataval not in self.stack:
-        self.stack.append(dataval)
-        return True
-      else:
-        return False
-
-    # Use peek to look at the top of the stack
-    def peek(self):     
-      return self.stack[0]
-
-    # Use list pop method to remove element
-    def remove(self):
-      if len(self.stack) <= 0:
-        return ("No element in the Stack")
-      else:
-        return self.stack.pop()
       
 # set inititial cords 
 print("What are your GPS cordinates?")
